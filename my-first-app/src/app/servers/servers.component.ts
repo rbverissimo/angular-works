09abe7556d72ded0c47e22ec9a10f18237da2c08
rbverissimo <rverissimo.dev@gmail.com>
2023-03-23 06:52:33 -0300

    Using templates are mandotory for an Angular component to work. So we can do it through component file by passing the templateUrl in the @Component decorator. Or we can do it by simply writing template the html-angular(ish) tags that we want to build that template

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  template: ` <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
