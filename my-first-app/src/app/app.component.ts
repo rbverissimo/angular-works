0556363015b972f8809f4e0b64569c48db71de33
rbverissimo <rverissimo.dev@gmail.com>
2023-03-23 07:06:38 -0300

    Understanding backticks and how Angular styles in-line in the app.component works

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  // styleUrls: ['./app.component.css'],
+  styles: [
+    `
+      h1 {
+        color: dodgerblue;
+      }
+      p {
+        color: blue;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   name = '';
