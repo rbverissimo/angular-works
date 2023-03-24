aa9f3d34ac4a2d5322d03e7db6cec2820ad0ccfd
rbverissimo <rverissimo.dev@gmail.com>
2023-03-24 07:48:55 -0300

    Adding the feature of using a method for String interpolation. Adding a button to the servers component that lets the use add a new server. This new server will use the logic that has been implemented in the server component with the String interpolation

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -4,11 +4,14 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: ` <app-server></app-server>
-    <app-server></app-server>`,
+  /* template: ` <app-server></app-server>
+    <app-server></app-server>`, */
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+
   constructor() {}
 
   ngOnInit(): void {}
