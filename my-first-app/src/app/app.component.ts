6e27439690398756fa05211ad1f65ce36c7c671d
rbverissimo <rverissimo.dev@gmail.com>
2023-03-27 07:15:26 -0300

    Solved the damn fucking bug! Everything is working as expected. Solved it by uncommenting the styleUrls in the app.component file

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css'],
   styles: [
     `
       h1 {
@@ -23,6 +23,7 @@ export class AppComponent {
 
   onTogglePassword() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
