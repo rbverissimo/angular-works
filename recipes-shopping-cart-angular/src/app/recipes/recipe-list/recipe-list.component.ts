d951c28de428c31c3908a10b0b206614fc5e0192
rbverissimo <rverissimo.dev@gmail.com>
2023-03-29 20:11:52 -0300

    Using ngFor inside an <a> tag to create a list of recipes. Bootstrap isn't really display my div classes right.

--- recipes-shopping-cart-angular/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes-shopping-cart-angular/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
     ),
   ];
 
