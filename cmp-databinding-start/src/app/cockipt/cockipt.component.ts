7b8b0589b3d94b328b941304f80739907b2d006a
rbverissimo <rverissimo.dev@gmail.com>
2023-04-01 18:30:31 -0300

    Time to organize the application with new components.

---
+++ cmp-databinding-start/src/app/cockipt/cockipt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockipt',
+  templateUrl: './cockipt.component.html',
+  styleUrls: ['./cockipt.component.css']
+})
+export class CockiptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
