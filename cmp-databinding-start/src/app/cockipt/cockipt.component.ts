bdbce28f969d914d52759edaa7c55840c6d42660
rbverissimo <rverissimo.dev@gmail.com>
2023-04-02 19:49:54 -0300

    Got myself where i needed to be: to understand how components communicate to each other

--- cmp-databinding-start/src/app/cockipt/cockipt.component.ts
+++ cmp-databinding-start/src/app/cockipt/cockipt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockipt',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockiptComponent implements OnInit {
 
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string, serverContent: string}>();
+
+  @Output() blueprintCreated = new EventEmitter<{
+    serverName: string, serverContent: string}>();
   serverElements = [];
   newServerName = '';
   newServerContent = ''; 
@@ -17,19 +22,15 @@ export class CockiptComponent implements OnInit {
   }
 
   onAddServer() {
-  /*    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    }); */
+    this.serverCreated.emit({
+      serverName: this.newServerName, 
+      serverContent: this.newServerContent});
   }
 
   onAddBlueprint() {
-/*     this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    }); */
+    this.blueprintCreated.emit(
+      {serverName: this.newServerName,
+      serverContent: this.newServerContent})
   }  
 
 }
