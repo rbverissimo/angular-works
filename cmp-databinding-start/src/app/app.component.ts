5ca9b4fd37f88a1795a7801c03ca35a1607a1f32
rbverissimo <rverissimo.dev@gmail.com>
2023-04-02 18:44:47 -0300

    Solving bugs from the last commit. Transfering every method and attibrute from the app.componet to the cockpit.component. So that the html file can consume this components.

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -5,24 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-}
+export class AppComponent { }
--- cmp-databinding-start/src/app/cockipt/cockipt.component.ts
+++ cmp-databinding-start/src/app/cockipt/cockipt.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockiptComponent implements OnInit {
 
+  serverElements = [];
+  newServerName = '';
+  newServerContent = ''; 
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }  
 
 }
