55098e37c990fd21aeef1c496deb1a227e8cbdbd
rbverissimo <rverissimo.dev@gmail.com>
2023-04-02 18:59:59 -0300

    Moved more code to the server-element component. Also, add some style to the app component. Everythin is working as intended.

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
+  serverElements = [];
+
   constructor() { }
 
   ngOnInit(): void {
