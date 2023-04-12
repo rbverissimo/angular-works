62bc776a4d21f5a19526a87a9668b2718f13e848
rbverissimo <rverissimo.dev@gmail.com>
2023-04-12 06:27:52 -0300

    In the header component: adding a click event that will select a feature: either recipe or shopping-list. the onSelect method emits an event listenable from the outside of the component.

--- recipes-shopping-cart-angular/src/app/header/header.compont.ts
+++ recipes-shopping-cart-angular/src/app/header/header.compont.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+
+  @Output() featureSelect = new EventEmitter<string>();
+
+  onSelect(feature: string){
+    this.featureSelect.emit(feature);
+  }
 }
