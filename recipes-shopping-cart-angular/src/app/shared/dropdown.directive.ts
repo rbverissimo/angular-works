96571501ce9347e43246d67425e3fe34dd89452b
rbverissimo <rverissimo.dev@gmail.com>
2023-04-26 19:44:32 -0300

    Building a simple and effective way to let just one dropdown active in the DOM, closing any other one if open.

--- recipes-shopping-cart-angular/src/app/shared/dropdown.directive.ts
+++ recipes-shopping-cart-angular/src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
@@ -7,10 +7,11 @@ export class DropdownDirective {
 
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen(){
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event){
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ?
+    !this.isOpen : false;
   }
 
-  constructor() { }
+  constructor(private elRef: ElementRef) { }
 
 }
