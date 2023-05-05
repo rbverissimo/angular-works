dff5134c4e5b8d6cf19c997d721b7559e326edd5
rbverissimo <rverissimo.dev@gmail.com>
2023-05-05 19:47:36 -0300

    Removing the @Injectable: in root decorator from the Services and writing them in the providers array in the app module which produce the same effect and lets me inject one service into another service.

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -5,8 +5,7 @@ import { AccountsService } from '../shared/accounts.service';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -5,8 +5,7 @@ import { AccountsService } from '../shared/accounts.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
 
