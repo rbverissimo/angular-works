26d47e2e59fff6174891ecabe6bfc8f82d56471c
rbverissimo <rverissimo.dev@gmail.com>
2023-05-09 07:54:22 -0300

    I added the recipes list to the recipe service and made it private. It is only accessible through the getRecipes method which returns not an reference to the array BUT the actual array itself. I made it possible by passing a slice call to the array

--- recipes-shopping-cart-angular/src/app/recipes/recipe.service.ts
+++ recipes-shopping-cart-angular/src/app/recipes/recipe.service.ts
@@ -1,1 +1,22 @@
-export class RecipeService{}
+import { Recipe } from "./recipe.model";
+
+export class RecipeService{
+
+private recipes: Recipe[] = [
+      new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+            ),
+      new Recipe(
+            'My second Recipe',
+            'Description to the second recipe',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+            )
+      ];
+
+getRecipes() {
+      return this.recipes.slice();
+}      
+
+}
