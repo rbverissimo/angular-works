de00a2e8b34bd59e486cf693ec6412225410109d
rbverissimo <rverissimo.dev@gmail.com>
2023-05-10 06:09:11 -0300

    Implementing ingredients in the recipe-detail. For that, the recipe must have a state as any object needs to have. Also, it needs to pass down this state to an <ul> in the recipe detail

--- recipes-shopping-cart-angular/src/app/recipes/recipe.service.ts
+++ recipes-shopping-cart-angular/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService{
 
@@ -7,14 +8,22 @@ recipeSelected = new EventEmitter<Recipe>();
 
 private recipes: Recipe[] = [
       new Recipe(
-            'A Test Recipe',
-            'This is simply a test',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+            'Tasty Schnitzel',
+            'A super-tasty Schnitzel - just awesome!',
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG', 
+            [
+                  new Ingredient('Meat', 1),
+                  new Ingredient('French Fries', 20),
+            ]
             ),
             new Recipe(
-            'My second Recipe',
-            'Description to the second recipe',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+            'Big Fat Burger',
+            'What else you need to say?',
+            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg', 
+            [
+                  new Ingredient('Buns', 2),
+                  new Ingredient('Meat', 1)
+            ]
             )
       ];
 
