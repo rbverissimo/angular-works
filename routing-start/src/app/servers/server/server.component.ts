787bae312386bb6a61233b60e9d648bf0ce45d30
rbverissimo <rverissimo.dev@gmail.com>
2023-06-17 11:10:30 -0300

    Solving an issue regarding the id in the params. They come by as string literals, whereas, i need a number. So by adding just the plus sign at the beginning can convert it to a number. Also, I commented the single app-server in the servers component.

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -15,7 +15,7 @@ export class ServerComponent implements OnInit {
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params
     .subscribe((params: Params) => {
