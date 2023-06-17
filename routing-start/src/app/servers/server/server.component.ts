846e1fe55f078c5dad63f47f78e64afccf974a0a
rbverissimo <rverissimo.dev@gmail.com>
2023-06-17 11:06:58 -0300

    Adding a subscribe to whener the params['id'] changes

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -15,9 +15,12 @@ export class ServerComponent implements OnInit {
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log(this.route.snapshot)
     const id = this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
+    this.route.params
+    .subscribe((params: Params) => {
+      this.server = this.serversService.getServer(params['id']);  
+    });
   }
 
 }
