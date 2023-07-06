2aa6e8f23e31d328d8ab9859ad1aa4d90a1de3d0
rbverissimo <rverissimo.dev@gmail.com>
2023-07-05 21:06:50 -0300

    Creating a new ErrorPage that should receive data dynamically in order to pass a message to the user. To do that I used the data object on the path declaration. This data object receives the information to pass down to the component via the ActivatedRoute params using the subscribe method.

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 
 const appRoutes: Routes = [
@@ -25,7 +26,8 @@ const appRoutes: Routes = [
             {path: ':id/edit', component: EditServerComponent, 
                   canDeactivate: [CanDeactivateGuard] },    
       ] },
-      {path: 'not-found', component: PageNotFoundComponent},
+      // {path: 'not-found', component: PageNotFoundComponent},
+      {path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'}},
       {path: '**', redirectTo: '/not-found'}
 ];
 
