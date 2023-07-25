d6245a818a4fe83c323ee369efd36ec818db6818
rbverissimo <rverissimo.dev@gmail.com>
2023-07-24 23:49:06 -0300

    Adding the routerLink property to the anchors so that the header navigation is finally enabled

--- recipes-shopping-cart-angular/src/app/header/header.compont.ts
+++ recipes-shopping-cart-angular/src/app/header/header.compont.ts
@@ -7,10 +7,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
-
-  @Output() featureSelect = new EventEmitter<string>();
-
-  onSelect(feature: string){
-    this.featureSelect.emit(feature);
-  }
 }
