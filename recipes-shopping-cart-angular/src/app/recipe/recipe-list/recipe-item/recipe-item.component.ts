101a58c5b66c4e83f6a0fb572d94dbf9490bed7a
rbverissimo <rverissimo.dev@gmail.com>
2023-07-29 18:16:17 -0300

    Cleaning the way for setting the RecipeDetailComponent to receive that through queryParams

--- recipes-shopping-cart-angular/src/app/recipe/recipe-list/recipe-item/recipe-item.component.ts
+++ recipes-shopping-cart-angular/src/app/recipe/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/recipes/recipe.model';
-import { RecipeService } from 'src/app/recipes/recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,13 +10,8 @@ export class RecipeItemComponent implements OnInit {
   
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService){}
   ngOnInit(): void {
     
-  }
-
-  onSelected(){
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
