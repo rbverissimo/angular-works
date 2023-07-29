f88a045f16f6b21e391721b5724bf06faf4566ce
rbverissimo <rverissimo.dev@gmail.com>
2023-07-29 18:47:08 -0300

    There is something wrong!

--- recipes-shopping-cart-angular/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipes-shopping-cart-angular/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -20,6 +20,7 @@ export class RecipeDetailComponent implements OnInit {
     this.route.params
     .subscribe((params: Params) =>{
       this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
     });
   }
 
