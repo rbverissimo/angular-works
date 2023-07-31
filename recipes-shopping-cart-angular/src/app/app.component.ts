79aa66fba6c2ed5876838e700fb71f58c9744e08
rbverissimo <rverissimo.dev@gmail.com>
2023-07-31 19:13:54 -0300

    Cleaning up some junk in the AppComponent

--- recipes-shopping-cart-angular/src/app/app.component.ts
+++ recipes-shopping-cart-angular/src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipes-shopping-cart-angular';
-
-  loadedFeature = 'recipe-list';
-
-  onNavigate(feature: string){
-    this.loadedFeature = feature;
-  }
 }
