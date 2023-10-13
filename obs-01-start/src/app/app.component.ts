20e7c36adf7d31456f3d2ba673682019ccf56732
rbverissimo <rverissimo.dev@gmail.com>
2023-10-13 11:37:18 -0300

    Understanding Subjects and using them instead of EventEmitters

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  
+  private activatedSub: Subscription;
+  isUserActivated: boolean;
 
-  ngOnInit() {}
+
+  constructor(private userService: UserService) {}
+
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedSubject.subscribe( (didUserActivated: boolean) => {
+      this.isUserActivated = didUserActivated;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 }
