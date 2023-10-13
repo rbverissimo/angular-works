df591397e74e47be1c007de5e7fd47bcff4fef5b
rbverissimo <rverissimo.dev@gmail.com>
2023-10-13 10:56:46 -0300

    Doing some observable things

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -9,23 +9,31 @@ import { Observable, Subscription, interval } from 'rxjs';
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-  private firstObsSubscription: Subscription;
+  // private firstObsSubscription: Subscription;
   private secondObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(
+/*     this.firstObsSubscription = interval(1000).subscribe(
       count => {
         console.log(count)
       }
     );
-
+ */
     // Building an observable from scratch, defining its logic
     const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+
+        if(count === 2){
+          observer.complete();
+        }
+
+        if(count > 3){
+          observer.error(new Error('Counter is greater than 3!'));
+        }
         count++;
       }, 1000)
     });
@@ -33,11 +41,13 @@ export class HomeComponent implements OnInit, OnDestroy {
     // Subscribing to the  observable
     this.secondObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, err => {
+      // alert(err.message);
     });
   }
 
   ngOnDestroy(): void {
-    this.firstObsSubscription.unsubscribe();
+    // this.firstObsSubscription.unsubscribe();
     this.secondObsSubscription.unsubscribe();
   }
 
