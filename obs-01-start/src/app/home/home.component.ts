f9a373f93e07ef5560290376973e640a664021cd
rbverissimo <rverissimo.dev@gmail.com>
2023-10-13 11:12:21 -0300

    Working with some pipes and observables

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Observable, Subscription, interval } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -24,27 +25,40 @@ export class HomeComponent implements OnInit, OnDestroy {
     // Building an observable from scratch, defining its logic
     const customIntervalObservable = Observable.create(observer => {
       let count = 0;
+      const limit = 10;
       setInterval(() => {
         observer.next(count);
 
-        if(count === 2){
+        if(count === 12){
           observer.complete();
         }
 
-        if(count > 3){
-          observer.error(new Error('Counter is greater than 3!'));
+        if(count > limit){
+          observer.error(new Error(`Counter is greater than ${limit}!`));
         }
         count++;
       }, 1000)
     });
 
     // Subscribing to the  observable
-    this.secondObsSubscription = customIntervalObservable.subscribe(data => {
-      console.log(data);
-    }, err => {
-      // alert(err.message);
-    });
+    this.secondObsSubscription = customIntervalObservable
+    .pipe(
+      filter((data: number) => {
+        return data % 2 === 0;
+      }),
+      map((data: number) => 
+      {
+        return 'Round: ' + (data + 1);  
+      }))
+      .subscribe(data => 
+        {
+          console.log(data);
+        }, err => {
+        // alert(err.message);
+      });
   }
+
+
 
   ngOnDestroy(): void {
     // this.firstObsSubscription.unsubscribe();
