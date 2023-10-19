f6a54a05399768bad6cd2f13329490e5a9a0ea3e
rbverissimo <rverissimo.dev@gmail.com>
2023-10-19 05:49:44 -0300

    Doing some cleanup after changing from EventEmitters to Subjects

--- recipes-shopping-cart-angular/src/app/shopping-list/shopping-list.component.ts
+++ recipes-shopping-cart-angular/src/app/shopping-list/shopping-list.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscriptionIngredientsChanged: Subscription;
 
   constructor(private slService: ShoppingListService){}
 
   ngOnInit(){
     this.ingredients = this.slService.getIngredients();
 
-    this.slService.ingredientsChanged.subscribe(
+    this.subscriptionIngredientsChanged = this.slService.ingredientsChanged
+    .subscribe(
       (i: Ingredient[]) => {
         this.ingredients = i;
       }
       );
+  }
 
+  ngOnDestroy(): void {
+    this.subscriptionIngredientsChanged.unsubscribe();
   }
 
 }
--- recipes-shopping-cart-angular/src/app/shopping-list/shopping-list.service.ts
+++ recipes-shopping-cart-angular/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,9 @@
-import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { Subject } from 'rxjs';
 
 export class ShoppingListService{
 
-      ingredientsChanged = new EventEmitter<Ingredient[]>();
+      ingredientsChanged = new Subject<Ingredient[]>();
 
       private ingredients: Ingredient[] = [
                   new Ingredient('Test Ingredient', 10),
@@ -17,13 +17,13 @@ export class ShoppingListService{
 
       addIngredient(ingredient: Ingredient){
             this.ingredients.push(ingredient);
-            this.ingredientsChanged.emit(this.ingredients.slice());
+            this.ingredientsChanged.next(this.ingredients.slice());
       }
 
       addIngredients(ingredients: Ingredient[]){
             // use of the spread operator;
             this.ingredients.push(...ingredients);
-            this.ingredientsChanged.emit(this.ingredients.slice());
+            this.ingredientsChanged.next(this.ingredients.slice());
       }
 
 }
